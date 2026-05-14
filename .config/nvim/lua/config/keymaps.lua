f2df45a3b598aa917a42d8637b25462ce0423a29
sandemiroren <sandemiroren1@gmail.com>
2026-05-14 09:20:34 +0200

    debugger

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- DAP (debugger)
+map("n", "<leader>dt", function() require("dap").toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+map("n", "<leader>dc", function() require("dap").continue() end, { desc = "Continue" })
+map("n", "<leader>di", function() require("dap").step_into() end, { desc = "Step Into" })
+map("n", "<leader>do", function() require("dap").step_over() end, { desc = "Step Over" })
+map("n", "<leader>du", function() require("dap").step_out() end, { desc = "Step Out" })
+map("n", "<leader>dr", function() require("dap").repl.open() end, { desc = "Open REPL" })
+map("n", "<leader>dl", function() require("dap").run_last() end, { desc = "Run Last" })
+map("n", "<leader>dq", function() require("dap").terminate() end, { desc = "Terminate" })
+map("n", "<leader>db", function() require("dap").list_breakpoints() end, { desc = "List Breakpoints" })
+map("n", "<leader>de", function() require("dap").set_exception_breakpoints() end, { desc = "Exception Breakpoints" })
---
+++ .config/nvim/lua/plugins/debugger.lua
@@ -0,0 +1,86 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "jay-babu/mason-nvim-dap.nvim",
+    "theHamsta/nvim-dap-virtual-text",
+  },
+  config = function()
+    local mason_dap = require("mason-nvim-dap")
+    local dap = require("dap")
+    local ui = require("dapui")
+    local dap_virtual_text = require("nvim-dap-virtual-text")
+
+    -- Dap Virtual Text
+    dap_virtual_text.setup()
+
+    mason_dap.setup({
+      ensure_installed = { "cppdbg" },
+      automatic_installation = true,
+    })
+
+    -- Configurations
+    local cpp_configs = {
+      {
+        name = "Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopAtEntry = false,
+        MIMode = "gdb",
+        miDebuggerPath = "/usr/bin/gdb",
+        setupCommands = {
+          {
+            text = "-enable-pretty-printing",
+            description = "enable pretty printing",
+            ignoreFailures = false,
+          },
+        },
+      },
+      {
+        name = "Attach to gdbserver :1234",
+        type = "cppdbg",
+        request = "launch",
+        MIMode = "gdb",
+        miDebuggerPath = "/usr/bin/gdb",
+        miDebuggerServerAddress = "localhost:1234",
+        cwd = "${workspaceFolder}",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+      },
+    }
+
+    dap.adapters.cppdbg = {
+      id = "cppdbg",
+      type = "executable",
+      command = vim.fn.stdpath("data") .. "/mason/bin/OpenDebugAD7",
+    }
+
+    dap.configurations.c = cpp_configs
+    dap.configurations.cpp = cpp_configs
+
+    -- Dap UI
+
+    ui.setup()
+
+    vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "DiagnosticError" })
+
+    dap.listeners.before.attach.dapui_config = function()
+      ui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      ui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      ui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      ui.close()
+    end
+  end,
+}
