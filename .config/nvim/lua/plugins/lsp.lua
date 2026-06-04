7ca902591d0de03be80dcf7b883d7e55c073509c
sandemiroren <sandemiroren1@gmail.com>
2026-06-04 23:06:16 +0200

    i3

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      kotlin_language_server = {},
       pyright = {},
       clangd = {},
       hls = {},
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,6 +3,7 @@ return {
   opts = {
     ensure_installed = {
       "typescript-language-server",
+      "kotlin-lsp",
       "svelte-language-server",
       "prettier",
       "eslint_d",
